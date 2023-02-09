212cb7cbdb64dc416cc512ca755b18e5341fefb6
ferrodri <f.rodriguez.hervias@gmail.com>
2023-02-09 10:21:29 +0100

    Initial commit

--- interface/components/Account.js
+++
@@ -1,8 +0,0 @@
-import { useAccount } from 'wagmi';
-import { Text } from '@chakra-ui/react';
-
-export function Account() {
-    const { address } = useAccount();
-
-    return (<Text padding='10px'> Welcome {address} </Text>);
-}
